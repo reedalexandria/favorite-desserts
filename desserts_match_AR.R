73564f68ca0c0363e13c359235c6c528a79c1714
reedalexandria <reed.alexandria@outlook.com>
2022-08-23 22:07:44 +0000

    update

--- desserts_match_AR.R
+++ desserts_match_AR.R
@@ -14,4 +14,59 @@ match <- iconic_desserts |>
     TRUE ~ "No match"
   ))
 
-filter(dessert %in% str_detect(favorite_desserts[2,3], c("Brookie","Chocolate cake", "Crepe"))
+# Erika example:
+
+library(tidyverse)
+library(here)
+library(janitor)
+
+iconic <- read_csv(here("iconic_desserts.csv")) # file to compare favorites to
+fave <- read_csv(here("favorite_desserts.csv")) %>%
+  clean_names() # file with no match
+fave_test <- read_csv(here("favorite_desserts_test.csv")) %>%
+  clean_names() # file with a match
+
+
+# Test with no matches
+matches <- c()
+
+for (i in seq_along(fave$favorite_dessert)) {
+  for (j in seq_along(iconic$dessert)) {
+    if (fave$favorite_dessert[i] == iconic$dessert[j]) {
+      matches <- append(matches, fave$favorite_dessert[i])
+    }
+  }
+}
+
+if (length(matches) == 0) {
+  message("No matches.")
+}
+
+if (length(matches) != 0) {
+  print(matches_test)
+}
+
+# should output No matches.
+
+# Test with a match
+matches_test <- c()
+
+
+for (i in seq_along(fave_test$favorite_dessert)) {
+  for (j in seq_along(iconic$dessert)) {
+    if (fave_test$favorite_dessert[i] == iconic$dessert[j]) {
+      matches_test <- append(matches_test, fave_test$favorite_dessert[i])
+    }
+  }
+}
+
+
+if (length(matches_test) == 0) {
+  message("No matches.")
+}
+
+if (length(matches_test) != 0) {
+  print(matches_test)
+}
+
+# Should output "Shave ice" or whatever match is in your file
